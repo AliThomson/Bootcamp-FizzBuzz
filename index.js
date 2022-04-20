508f66973e7bb72b4148d4b75f1f6b3b290289ee
Alison Thomson <Alison.Thomson@softwire.com>
2022-04-20 12:19:19 +0100

    Add user input for rules to implement

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const prompt = require('prompt-sync')({sigint: true});
 
 const upperLimit = prompt('How high shall we go?');
+const rules = prompt('What rules shall we implement?')
+const rulesArray = rules.split(",");
 
 function isDivisbleBy(n,divisor) {
     return n % divisor === 0;
@@ -8,30 +10,42 @@ function isDivisbleBy(n,divisor) {
 for (let i = 1; i<=Number(upperLimit); i++){
     let outputArr = [];
     let fezz = "";
-    if (isDivisbleBy(i,3)) {
-        outputArr.push("Fizz");
-    }
-    if (isDivisbleBy(i,13)) {
-        outputArr.push("Fezz");
-        fezz = "Fezz";
-    }
-    if (isDivisbleBy(i,5)) {
-        outputArr.push("Buzz");
-    }
-    if (isDivisbleBy(i,7)) {
-        outputArr.push("Bang");
-    }
-    if (isDivisbleBy(i,11)) {
 
-        outputArr= ["Bong"];
-        if (fezz){
-            outputArr.unshift("Fezz");
+    if (rulesArray.includes('3')) {
+        if (isDivisbleBy(i, 3)) {
+            outputArr.push("Fizz");
+        }
+    }
+    if (rulesArray.includes('13')) {
+        if (isDivisbleBy(i, 13)) {
+            outputArr.push("Fezz");
+            fezz = "Fezz";
+        }
+    }
+    if (rulesArray.includes('5')) {
+        if (isDivisbleBy(i,5)) {
+            outputArr.push("Buzz");
+        }
+    }
+    if (rulesArray.includes('7')) {
+        if (isDivisbleBy(i,7)) {
+            outputArr.push("Bang");
+        }
+    }
+    if (rulesArray.includes('11')) {
+        if (isDivisbleBy(i,11)) {
+            outputArr = ["Bong"];
+            if (fezz) {
+                outputArr.unshift("Fezz");
+            }
         }
     }
 
-    if (isDivisbleBy(i,17)) {
-        outputArr.reverse();
-    }
+    if (rulesArray.includes('17')) {
+            if (isDivisbleBy(i,17)) {
+                outputArr.reverse();
+            }
+        }
     if (outputArr.length === 0) {
         outputArr = [i];
     }
