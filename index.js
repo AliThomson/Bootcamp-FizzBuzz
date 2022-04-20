a272340a04f22674b556c7a8306cff58f980809e
Alison Thomson <Alison.Thomson@softwire.com>
2022-04-20 11:17:58 +0100

    Add FezzBongBang

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-for (let i = 1; i<=100; i++){
-    let output = "";
-    if (i % 3 === 0) {
-        output = "Fizz";
+function isDivisbleBy(n,divisor) {
+    return n % divisor === 0;
+}
+for (let i = 1; i<=200; i++){
+    let outputArr = [];
+    let fezz = "";
+    if (isDivisbleBy(i,3)) {
+        outputArr.push("Fizz");
     }
-    if (i % 5 === 0) {
-        output += "Buzz";
+    if (isDivisbleBy(i,13)) {
+        outputArr.push("Fezz");
+        fezz = "Fezz";
     }
-    if (!output) {
-        output = i;
+    if (isDivisbleBy(i,5)) {
+        outputArr.push("Buzz");
     }
-    console.log(output);
+    if (isDivisbleBy(i,7)) {
+        outputArr.push("Bang");
+    }
+    if (isDivisbleBy(i,11)) {
+        outputArr= [fezz + "Bong"];
+    }
+
+    if (isDivisbleBy(i,17)) {
+        outputArr.reverse();
+    }
+    if (outputArr.length === 0) {
+        outputArr = [i];
+    }
+    let strResult = outputArr.toString();
+    strResult = strResult.replace(/,/g, '');
+    console.log(strResult);
 }
